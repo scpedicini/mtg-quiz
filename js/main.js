db6a0686bc17f7bf4e696734fd8ff0cea5fe66fb
Shaun Pedicini <scpedicini@gmail.com>
2020-11-24 11:57:22 -0500

    Minor name changes

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ let ProgressBar;
 // TODO Highscores
 
 // easy way to strip out console log statements in "release mode"
-console.log = () => { };
+// console.log = () => { };
 
 // device detection
 const isMobile = (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
